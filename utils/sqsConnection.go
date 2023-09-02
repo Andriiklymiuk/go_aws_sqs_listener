5e3b2445280b41d11529761a379c8db9bb51650c
Andrii Klymiuk <gradli.inc@gmail.com>
2023-09-02 13:38:47 +0300

    Update log

--- utils/sqsConnection.go
+++ utils/sqsConnection.go
@@ -30,7 +30,7 @@ func (c *SqsConnection) PollMessages() {
 		if err != nil {
 			fmt.Printf("failed to fetch sqs message %s\n", err)
 		} else {
-			fmt.Println(GreenColor, "aws sqs connection is ok", WhiteColor)
+			fmt.Println(BlueColor, "waiting for sqs messages", WhiteColor)
 		}
 
 		for _, message := range output.Messages {
